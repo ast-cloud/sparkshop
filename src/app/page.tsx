e04ba0b5739a653346f44516aed877667bc35874
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2024-03-25 22:47:50 +0530

    Changed newsletter section height in large screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <ScrollableProductContainer containerHeading="New products"/>
       </div>
 
-      <div className="mt-20 mb-0 lg:py-10 px-5 lg:px-0 h-48 lg:h-40 flex flex-col lg:flex-row justify-evenly" style={{backgroundColor:'#F4F1EA'}}>
+      <div className="mt-20 mb-0 lg:py-10 px-5 lg:px-0 h-48 lg:h-36 flex flex-col lg:flex-row justify-evenly" style={{backgroundColor:'#F4F1EA'}}>
         <div className="flex flex-col self-center">
           <Typography variant="h6">Spark Shop newsletter</Typography>
           <Typography variant="body2">New products, limited editions, event invitations and much more.</Typography>
