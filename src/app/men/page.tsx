6ffa758f3dec793ddab6811840e8ef4a07eb69d7
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2024-04-01 23:39:51 +0530

    Mens clothing page UI

--- src/app/men/page.tsx
+++ src/app/men/page.tsx
@@ -1,8 +1,16 @@
+import { Divider, Typography } from "@mui/material";
 
 export default function Men(){
     return (
-        <div>
-            Men clothing
+        <div className="flex justify-center ">
+            <div className="flex flex-col mt-12" style={{width:'85vw'}}>
+                <Typography variant="h5">Men Clothing</Typography>
+                <Divider flexItem color="#FBD139"  className="my-2" sx={{width:'8vw', height:'2px'}}/>
+                <p className="my-2" style={{alignSelf:'flex-end'}}>Sort By</p>
+                <div className="flex">
+
+                </div>
+            </div>
         </div>
     );
 }
