217a07956cde7a56fe36a527ea77bc4bf117eb40
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2024-04-02 16:34:56 +0530

    Sort By dropdown UI

--- src/app/men/SortbyDropdown.tsx
+++ src/app/men/SortbyDropdown.tsx
@@ -1,21 +1,32 @@
 'use client';
 import {FormControl, InputLabel, MenuItem, Select} from '@mui/material';
+import { useState } from 'react';
+import { SelectChangeEvent } from '@mui/material';
 
 export default function SortbyDropdown(){
+
+    const [sortbyValue, setSortbyValue] = useState('0');
+    const handleChange = (event: SelectChangeEvent) => {
+        setSortbyValue(event.target.value);
+      };
+
     return (
-        <FormControl>
+        <FormControl sx={{width:'150px', border:'0px solid red'}} >
             <InputLabel id="demo-simple-select-label">Sort By</InputLabel>
             <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                value={10}
+                value={sortbyValue}
                 label="Sort By"
-                onChange={()=>{}}
+                onChange={handleChange}
                 size='small'
+                className='w-full'
+                sx={{fontSize:'10px'}}
             >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value={'0'}>Popular</MenuItem>
+                <MenuItem value={'1'}>New</MenuItem>
+                <MenuItem value={'2'}>Price (Low to high)</MenuItem>
+                <MenuItem value={'3'}>Price (High to low)</MenuItem>
             </Select>
         </FormControl>
     );
