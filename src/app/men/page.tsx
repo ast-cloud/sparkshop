216bd08b7c286580969f53988260e30022966455
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2024-04-03 11:15:26 +0530

    Men clothing filter section UI

--- src/app/men/page.tsx
+++ src/app/men/page.tsx
@@ -15,35 +15,44 @@ export default function Men(){
                     <SortbyDropdown/>
                 </div>
                 <div className="flex mt-5">
-                    <div className="flex flex-col">
+                    <div className="flex flex-col " style={{width:'12vw', border:'0px solid red'}}>
                         <p className="mb-2 ml-5" style={{fontWeight:'bold'}}>Filters</p>
                         <Accordion elevation={0} className='' sx={{border:'none'}} disableGutters>
-                            <AccordionSummary sx={{fontSize:'10px', margin:0, border:'none'}}>
+                            <AccordionSummary sx={{fontSize:'10px', margin:0, border:'1px solid red', height: '40px', minHeight:'10px'}}>
                                 <div className="flex justify-between w-full">
                                     <Typography>Category</Typography>
                                     <KeyboardArrowDown sx={{color:'grey'}}/>
                                 </div>
                             </AccordionSummary>
-                            <AccordionDetails>
+                            <AccordionDetails className="pt-0" sx={{border:'1px solid green'}}>
                             <div className="flex flex-col">
-                                <FormControlLabel control={<Checkbox />} label="Checkbox 1" />
-                                <FormControlLabel control={<Checkbox />} label="Checkbox 2" />
-                                <FormControlLabel control={<Checkbox />} label="Checkbox 3" />
+                                <Typography variant="body2" className="pb-1">Topwear</Typography>
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">T-Shirts</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">Shirts</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">Kurta</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">Hoodies</Typography>} />
+                                <Typography variant="body2" className="py-1">Bottomwear</Typography>
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">Shorts</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">Jeans</Typography>} />
                             </div>
                             </AccordionDetails>
                         </Accordion>
                         <Accordion elevation={0} className='' sx={{border:'none'}} disableGutters>
-                            <AccordionSummary sx={{margin:0, border:'none'}}>
+                            <AccordionSummary sx={{fontSize:'10px', margin:0, border:'none', height:'40px', minHeight:'10px'}}>
                                 <div className="flex justify-between w-full">
                                     <Typography>Sizes</Typography>
                                     <KeyboardArrowDown sx={{color:'grey'}}/>
                                 </div>
                             </AccordionSummary>
-                            <AccordionDetails>
+                            <AccordionDetails className="pt-0">
                             <div className="flex flex-col">
-                                <FormControlLabel control={<Checkbox />} label="Checkbox A" />
-                                <FormControlLabel control={<Checkbox />} label="Checkbox B" />
-                                <FormControlLabel control={<Checkbox />} label="Checkbox C" />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">XS</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">S</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">M</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">L</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">XL</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">XXL</Typography>} />
+                                <FormControlLabel control={<Checkbox size="small" sx={{height:'10px', border:'0px solid blue'}}/>} label={<Typography variant="body2">XXXL</Typography>} />
                             </div>
                             </AccordionDetails>
                         </Accordion>
